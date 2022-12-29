015ac70236b70a470a5edd80fc922ce44c4b08d6
ze37 <josealvess6154@gmail.com>
2022-12-29 20:54:29 -0300

    Estudando sobre objetos em javascript

--- aula57.js
+++ aula57.js
@@ -1,11 +1,13 @@
-const caixa = document.querySelector("#caixa")
+class Pessoa{
+  constructor(nome,idade){
+    this.nome = nome
+    this.idade = idade
+  }
+}
 
-let ul = `<ul>`
-const carros = ["gol","palio","fiat","toyota"]
-carros.map((el)=>{
-  ul += `<li>${el}</li>`
-})
-ul+  `</ul>`
-
-
-caixa.innerHTML = ul 
+let p1 =new Pessoa("jose",39)
+let p2 =new Pessoa("ana",23)
+let p3 = new Pessoa("luana",19)
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(`a pessoa ${p3.nome} tem ${p3.idade} de idades`)
